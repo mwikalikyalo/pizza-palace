9438e05a8d891f56edf19a667861dd016fed5bd6
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2021-11-28 23:54:28 +0300

    It will add the updated toppings list

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-var crust= document.getElementById("crust");
-var pizzaSize= document.getElementById("pizza-size");
-var toppings= document.getElementById("pizza-toppings");
-
-var q1= prompt("Would you like for your pizza to be delivered to you?");
-if("q1===yes"){
-  confirm
-}
