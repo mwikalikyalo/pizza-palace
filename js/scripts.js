ec78984da4e1f0508482c47f33db17017022e64c
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2021-11-28 23:12:14 +0300

    It will add size prices

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var crust= document.getElementById("crust");
+var pizzaSize= document.getElementById("pizza-size");
+var toppings= document.getElementById("pizza-toppings");
+
+var q1= prompt("Would you like for your pizza to be delivered to you?");
+if("q1===yes"){
+  confirm
+}
